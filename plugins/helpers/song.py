b82e648d195f45f7fc1411025dee0c89fc93b385
ɴᴧʀᴜᴛᴏ ᴜᴢᴜᴍᴀᴋɪ <111524889+phoenix-monarch@users.noreply.github.com>
2023-02-09 21:09:02 +0530

    Update song.py

--- plugins/helpers/song.py
+++ plugins/helpers/song.py
@@ -49,7 +49,7 @@ def song(client, message):
         open(thumb_name, 'wb').write(thumb.content)
 
 
-        performer = f"[Elsa - 𝙲𝙸𝙽𝙴𝙼𝙰𝙻𝙰.𝙲𝙾𝙼™]" 
+        performer = f"[Elsa - PSYCHO NETWORK™]" 
         duration = results[0]["duration"]
         url_suffix = results[0]["url_suffix"]
         views = results[0]["views"]
@@ -66,7 +66,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '**𝚂𝚄𝙱𝚂𝙲𝚁𝙸𝙱𝙴 ›› [🍁 ᴄʜᴀɴɴᴇʟ 🍁](https://t.me/cinemala_com1)**\n**𝙿𝙾𝚆𝙴𝚁𝙴𝙳 𝙱𝚈 ›› [muѕíc вσч](https://t.me/CcommusicBot)**\n@𝙲𝙸𝙽𝙴𝙼𝙰𝙻𝙰.𝙲𝙾𝙼**'
+        rep = '**𝚂𝚄𝙱𝚂𝙲𝚁𝙸𝙱𝙴 ›› [🍁 ᴍᴜsɪᴄ ᴄʜᴀɴɴᴇʟ 🍁](https://t.me/myfavmusiccollection)**\n**ᴘᴏᴡᴇʀᴇᴅ ʙʏ -›› [ᴘsʏᴄʜᴏ ɴᴇᴛᴡᴏʀᴋ](https://t.me/psycho_association)**\n@PSYCHO-NETWORK**'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
