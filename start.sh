484a37ca81010755c0096656c3236d2bf33f3681
Psycho Association <124176508+The-Psycho-Association@users.noreply.github.com>
2023-02-10 18:30:20 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/phoenix-monarch/Elsa.git /Elsa
+  git clone https://github.com/The-Psycho-Association/WEDNESDAY.git /Elsa
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Elsa
 fi
 cd /Elsa
 pip3 install -U -r requirements.txt
-echo "𝙎𝙩𝙖𝙧𝙩𝙞𝙣𝙜 𝙀𝙡𝙨𝙖....🧞‍♂️"
+echo "𝙎𝙩𝙖𝙧𝙩𝙞𝙣𝙜 𝑾𝒆𝒅𝒏𝒆𝒔𝒅𝒂𝒚 ....🧞‍♂️"
 python3 bot.py
